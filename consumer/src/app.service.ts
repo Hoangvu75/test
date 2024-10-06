e3d84b248d27c5d0bd6bf78b699432c7966d48a8
Vu Huy Hoang <uzumakivuhuyhoang@gmail.com>
2024-10-06 12:19:19 +0000

    push

---
+++ consumer/src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { OrderDto } from './order.dto';
+
+@Injectable()
+export class AppService {
+  handleOrderPlaced(order: OrderDto) {
+    console.log(`Received: ${JSON.stringify(order)}`);
+    return {
+      message: 'Order placed successfully',
+      orderData: order,
+    };
+  }
+}
